69b07db30cbb170553ada810093e71230a3f9fdf
YusungKim <yusungkim@me.com>
2022-12-17 22:07:56 +0900

    search with enter key

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,13 +19,19 @@
 			goto(`/search/${searchTerm}`);
 		}
 	};
+
+	const searchIfEnter = (/** @type {{ key: string; }} */ e) => {
+		if (e.key == 'Enter') {
+			search()
+		}
+	}
 </script>
 
 <section class="w-full">
 	<h1 class="text-6xl">Search songs..</h1>
 	<div class="w-full pt-8 flex flex-col gap-1">
 		<div class="w-full relative">
-			<input class="p-2 text-xl rounded-sm w-full" type="text" bind:value={searchInputs} placeholder="Please input search term." />
+			<input class="p-2 text-xl rounded-sm w-full" type="text" bind:value={searchInputs} placeholder="Please input search term." on:keydown={searchIfEnter} />
 			<button class="absolute my-auto top-1 bottom-1 right-1 rounded-sm px-5 bg-slate-300 hover:bg-slate-400 hover:text-slate-50" on:click={search}>Search</button>
 		</div>
 	</div>
--- src/routes/search/[search_term]/+page.svelte
+++ src/routes/search/[search_term]/+page.svelte
@@ -12,13 +12,13 @@
 <h1 class="text-4xl pb-3">Results</h1>
 <section>
   <div class="grid grid-cols-2 2xl:grid-cols-3 gap-3">
-  {#each songs as song (song.trackId)}
-    <!-- <button on:click|self={()=>goto(`/search/${searchTerm}/${song.trackId}`)}>
+  {#each songs as song (song.trackId) }
+    <button on:click|self={()=>goto(`/search/${searchTerm}/${song.trackId}`)}>
       <SongCard {song} bind:allowedToPlay={allowedToPlayTrackId}/>
-    </button> -->
-    <a href={`/search/${searchTerm}/${song.trackId}`}>
+    </button>
+    <!-- <a href={`/search/${searchTerm}/${song.trackId}`} >
       <SongCard {song} bind:allowedToPlay={allowedToPlayTrackId}/>
-    </a>
+    </a> -->
   {/each}
 </div>
 </section>
